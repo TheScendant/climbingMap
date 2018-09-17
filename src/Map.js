5029dea045afce8df17720797b18a33072cc17c6
Hayden M Nier <hayden518@gmail.com>
2018-09-17 11:23:37 -0400

    pins on map.

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import geoMap from './geoMap';
+import geoMap from './geoJSONs/geoMap';
+import pins from './geoJSONs/locations';
 import * as d3 from 'd3';
 import './Map.css';
 
@@ -7,14 +8,17 @@ class Map extends Component {
   constructor(props) {
     super(props);
     this.createMap = this.createMap.bind(this);
+    this.createPins = this.createPins.bind(this);
     this.zoomFunction = this.zoomFunc.bind(this);
     window.d3 = d3; // console debugging
   }
   componentDidMount() {
-    this.createMap()
+    this.createMap();
+    this.createPins();
   }
   componentDidUpdate() {
-    this.createMap()
+    this.createMap();
+    this.createPins();
     console.warn('componentDidUpdate');
   }
   zoomFunc() {
@@ -22,9 +26,31 @@ class Map extends Component {
     const tString = `translate(${t.x}, ${t.y}) scale(${t.k})`;
     d3.select(this.countryPaths).attr("transform", tString);
   }
+  createPins() {
+    const pinsSel = d3.select(this.locationPins);
+    pinsSel
+      .selectAll("circle")
+      .data(pins.features) // dosomething make these more unique
+      .enter()
+      .append("circle")
+      pinsSel
+      .selectAll("circle")
+      .data(pins.features)
+      .exit()
+      .remove();
+
+      pinsSel
+      .selectAll("circle")
+      .data(pins.features)
+      .attr("class", "location-pin")
+      .attr("cx", d => this.projection(d.geometry.coordinates)[0])
+      .attr("cy", d => this.projection(d.geometry.coordinates)[1])
+      .attr("r", 5);
+
+  }
   createMap() {
-    const projection = d3.geoMercator().fitSize([this.svg.clientWidth, this.svg.clientHeight], geoMap);
-    const pathGenerator = d3.geoPath().projection(projection);
+    this.projection = d3.geoMercator().fitSize([this.svg.clientWidth, this.svg.clientHeight], geoMap);
+    this.pathGenerator = d3.geoPath().projection(this.projection);
     this.zoom = d3.zoom().on("zoom", this.zoomFunction)
     const svgSel = d3.select(this.svg);
     svgSel.call(this.zoom);
@@ -33,24 +59,26 @@ class Map extends Component {
       .selectAll('path')
       .data(geoMap.features)
       .enter()
-      .append('path')
+      .append('path');
 
     countPathsSel
       .selectAll('path')
       .data(geoMap.features)
       .exit()
-      .remove()
+      .remove();
 
     countPathsSel
       .selectAll('path')
       .data(geoMap.features)
-      .attr("d", d => pathGenerator(d));
+      .attr("d", d => this.pathGenerator(d));
   }
   render() {
     return (
-      <svg id="mapSVG" ref={svg => this.svg = svg} >
-        <g id="countryPaths" ref={g => this.countryPaths = g}></g>
-      </svg>);
+      <svg id="map-svg" ref={svg => this.svg = svg} >
+        <g id="g-country-paths" ref={g => this.countryPaths = g}></g>
+        <g id="g-location-pins" ref={g => this.locationPins = g}></g>
+      </svg>
+    );
   }
 }
 
