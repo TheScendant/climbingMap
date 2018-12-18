f59fbf44d8ff02ce50671ad15ce0d87c83692f76
Hayden M Nier <hayden518@gmail.com>
2018-12-18 17:39:39 -0500

    drop tooltip import

--- src/Map.js
+++ src/Map.js
@@ -4,7 +4,6 @@ import pins from './geoJSONs/locations';
 import cities from './geoJSONs/selectCities';
 import * as d3 from 'd3';
 import './Map.css';
-import Tooltip from './Tooltip';
 
 class Map extends Component {
   constructor(props) {
